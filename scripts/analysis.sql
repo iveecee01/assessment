bf6c02d58db553842e8d0b05525e5b1afdf49851
iveecee01 <v.c.igwe@gmail.com>
2024-03-27 21:10:48 -0400

    updates

--- scripts/analysis.sql
+++ scripts/analysis.sql
@@ -2,13 +2,13 @@
 -- It then searchs for adjoining tracts by looking for tracts that intersect
 -- There are 224 census tracts with coal generators retired after 2009
 -- There are 1750 cesus tracts that meet the coal closure conditions in section III
-with tracts_intersect as (
+WITH tracts_intersect AS (
     SELECT DISTINCT t.*
     FROM tracts t
-    JOIN generators_spatial g ON ST_Intersects(t.geom, g.geom)
+    LEFT JOIN generators_spatial g ON ST_Intersects(t.geom, g.geom)
     WHERE g."Technology" = 'Conventional Steam Coal' 
-    AND CAST("Retirement Year" AS int) >= 2009
+    AND CAST(g."Retirement Year" AS int) >= 2009
 )
-SELECT distinct t.*
-FROM tracts_intersect ti, tracts t
-WHERE ST_Intersects(ti.geom, t.geom);
+SELECT count(DISTINCT t.*)
+FROM tracts_intersect ti
+JOIN tracts t ON ST_Intersects(ti.geom, t.geom);
